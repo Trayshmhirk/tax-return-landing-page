5dd123c54fc623626ecae0f5513e506d3e0eb3f7
Frank Micheal <harlex.mikkey@gmail.com>
2024-09-05 14:28:12 +0100

    Set up footer. Add shadcn ui configuration.

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+   return <div className="w-full flex flex-col">Footer</div>;
+};
+
+export default Footer;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
 const MainLayout = () => {
    return (
       <div className="w-full h-full flex flex-col overflow-hidden">
          <Header />
          <Outlet />
+         <Footer />
       </div>
    );
 };
