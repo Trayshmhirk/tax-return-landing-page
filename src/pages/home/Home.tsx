5b51d5861a92db323652a5fad77d07d9750ede8b
Frank Micheal <harlex.mikkey@gmail.com>
2024-09-13 07:45:15 +0100

    Add home background image. other changes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,14 +4,25 @@ const Home = () => {
    return (
       <main className="flex flex-col">
          {/* hero */}
-         <section className="flex justify-center gap-6 bg-antiFlashWhite dark:bg-darkGray py-8 md:py-14 px-5 sm:px-8 lg:px-10">
-            <div className="container flex flex-col md:flex-row items-center gap-8">
+         <section
+            className="relative flex justify-center gap-6 bg-antiFlashWhite dark:bg-darkGray py-8 md:py-14 px-5 sm:px-8 lg:px-10"
+            style={{
+               backgroundImage:
+                  "url(https://i.postimg.cc/zfpKsQJk/istockphoto-1408404108-1024x1024-transformed.jpg)",
+               backgroundRepeat: "no-repeat",
+               backgroundSize: "cover",
+               backgroundPosition: "center",
+               backgroundPositionY: "80%",
+            }}
+         >
+            <div className="absolute inset-0 bg-gray opacity-60" />
+            <div className="container flex flex-col md:flex-row items-center gap-8 z-50">
                <div className="w-full md:w-[40%] xl:w-1/3 flex flex-col gap-5 md:gap-8 text-center md:text-left">
                   <div className="flex flex-col gap-4">
                      <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-richElectricBlue tracking-wide">
                         YOUR BEST <br /> TAX APP <br /> SOLUTIONS
                      </h1>
-                     <p className="max-w-96 md:max-w-full self-center md:self-start font-medium">
+                     <p className="max-w-96 md:max-w-full self-center md:self-start font-medium text-white">
                         Making a mistake on your return can be costly. From
                         missed deductions, or worse, the dreaded IRS audit.
                      </p>
@@ -36,13 +47,13 @@ const Home = () => {
                         <img
                            src="https://i.postimg.cc/R0dmt5q3/3ea21503-d952-4bd3-8453-c29194e8be4d.png"
                            alt="desktop web app image"
-                           className="absolute top-[2%] left-[11%] w-[78%] h-[72%] xs:h-[79%] sm:h-[87%] object-fill z-0 dark:hidden"
+                           className="absolute top-[2%] left-[10.5%] w-[79%] h-[72%] xs:h-[79%] sm:h-[87%] object-fill z-0 dark:hidden"
                         />
                         {/* Dark mode Desktop web app image (inside the MacBook screen) */}
                         <img
                            src="https://i.postimg.cc/zvLXZBd9/c8bc7e1b-4c3e-4f7e-bb88-a3a6c796f1e3.png"
                            alt="desktop web app image"
-                           className="absolute top-[2%] left-[11%] w-[78%] h-[72%] xs:h-[79%] sm:h-[87%] object-fill z-0 hidden dark:block"
+                           className="absolute top-[2%] left-[10.5%] w-[79%] h-[72%] xs:h-[79%] sm:h-[87%] object-fill z-0 hidden dark:block"
                         />
                      </div>
                   </div>
@@ -121,7 +132,7 @@ const Home = () => {
 
                <div className="w-full flex justify-center md:justify-start">
                   <img
-                     src="https://i.postimg.cc/FH9sLRvD/istockphoto-1408321925-1024x1024.jpg"
+                     src="https://i.postimg.cc/ThvDWx76/1408321925-1024x1024-transformed.jpg"
                      alt="image"
                      className="w-full max-w-md md:max-w-lg rounded-xl"
                   />
