2ed959ff0c855a7485549dc90c172d0e2927017b
Frank Micheal <harlex.mikkey@gmail.com>
2024-09-15 08:17:07 +0100

    minor changes

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,7 @@ import CustomThemeSwitch from "../custom/CustomThemeSwitch";
 
 const Header = () => {
    return (
-      <header className="bg-white flex justify-center items-center dark:bg-eerieBlack py-3 px-5 sm:px-7 lg:px-10">
+      <header className="bg-white flex justify-center items-center dark:bg-eerieBlack py-3 px-5 sm:px-7 lg:px-10 text-sm lg:text-base">
          <div className="w-full md:container flex items-center justify-between gap-4">
             <div className="flex items-center gap-3">
                <img
@@ -14,7 +14,7 @@ const Header = () => {
                   src="https://img.icons8.com/external-flatarticons-blue-flatarticons/65/external-tax-taxes-flatarticons-blue-flatarticons.png"
                   alt="external-tax-taxes-flatarticons-blue-flatarticons"
                />
-               <h1 className="md:text-xl font-bold">Tax Returns</h1>
+               <h1 className="text-base lg:text-xl font-bold">Tax Returns</h1>
             </div>
 
             <nav className="hidden items-center md:gap-7 lg:gap-10 px-3 md:flex">
